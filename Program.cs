dfc6b70276d943baef67f95c72d6fc98868edfdb
Daniel Kurzer <121629742+daniel-kurzer@users.noreply.github.com>
2024-04-22 09:09:04 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bitte geben Sie die erste Zahl ein:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Bitte geben Sie die zweite Zahl ein:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Bitte wählen Sie eine Operation (+, -, *, /):");
+        char operation = Convert.ToChar(Console.ReadLine());
+
+        double result = 0;
+
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0)
+                    result = num1 / num2;
+                else
+                    Console.WriteLine("Ungültige Division durch Null!");
+                break;
+            default:
+                Console.WriteLine("Ungültige Operation!");
+                break;
+        }
+
+        Console.WriteLine("Ergebnis: " + result);
+    }
+}
